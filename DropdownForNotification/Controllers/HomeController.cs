94296d9ab2b33fa4163f6b0a45be8f720a75d5c7
yusuf.boyaci <yusuf.boyaci@agarthateknoloji.com.tr>
2022-07-19 17:29:12 +0300

    Add project files.

---
+++ DropdownForNotification/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DropdownForNotification.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DropdownForNotification.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DropdownNotification()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DropdownForNotification/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DropdownForNotification.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
